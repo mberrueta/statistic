2c93c7fc9e5925de1dc57f4b2535581f1d35d1a5
matt <matiasberrueta@gmail.com>
2017-10-14 23:19:51 -0300

    Inverse poison

--- app/src/main/java/com/uade/matt/statistic/models/PoissonDistributionCalc.java
+++ app/src/main/java/com/uade/matt/statistic/models/PoissonDistributionCalc.java
@@ -13,6 +13,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
+import static com.uade.matt.statistic.utils.Helper.isNullorZero;
 import static com.uade.matt.statistic.utils.Helper.round;
 
 @Accessors(chain = true, fluent = true)
@@ -39,6 +40,32 @@ public class PoissonDistributionCalc extends DistributionCalc {
 
         mean = variance = n = round(frequency * t);
         dist = new PoissonDistribution(n);
+
+        if (isNullorZero(r)) {
+            if(!isNullorZero(f))
+            {
+                Integer r2 = dist.inverseCumulativeProbability(f);
+                Integer r1 = r2 - 1;
+                Double f1 = dist.cumulativeProbability(r1);
+                Double f2 = dist.cumulativeProbability(r2);
+                resultMessage = String.format("Range values: %n" +
+                        "P(r<%d) = %f %n" +
+                        "P(r<%d) = %f", r1, f1, r2, f2);
+                return this;
+            }
+            else
+            {
+                Integer r1 = dist.inverseCumulativeProbability(1 - g);
+                Integer r2 = r1 + 1;
+                Double g1 = round(dist.probability(r1) + (1 - dist.cumulativeProbability(r1)));
+                Double g2 = round(dist.probability(r2) + (1 - dist.cumulativeProbability(r2)));
+                resultMessage = String.format("Range values: %n" +
+                        "P(r>%d) = %f %n" +
+                        "P(r>%d) = %f", r1, g1, r2, g2);
+                return this;
+            }
+        }
+
 
         p = round(dist.probability(r));
         f = round(dist.cumulativeProbability(r));
